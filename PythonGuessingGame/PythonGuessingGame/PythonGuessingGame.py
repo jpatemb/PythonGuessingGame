85f8f20360d7074816de09b6074899d97d279861
jpatemb <51932270+jpatemb@users.noreply.github.com>
2021-06-18 23:00:05 -0400

    PythonGuessingGame

---
+++ PythonGuessingGame/PythonGuessingGame/PythonGuessingGame.py
@@ -0,0 +1,24 @@
+def game():
+    n = random.randint(1, 100)
+    count = 1
+    guess = int(input("I'm thinking of a number between 1 an 10. Can you guess what number it is?"))
+    while n != "guess":
+        if guess < n:
+            print ("Too low")
+            count += 1
+            guess = int(input("Guess again:"))
+        elif guess > n:
+            print("Too high")
+            count += 1
+            guess = int(input("Guess again:"))
+        elif guess == n:
+            print("Correct! You guessed the number in " + str(count) + " tries!")
+            response = input("Want to play again? (y for Yes, n for No)")
+            if response.lower().startswith("y"):
+                print("Ok! Then let's play again!")
+                game()
+            elif response.lower().startswith("n"):
+                print("Alright, on to something else then.")
+                break
+            else:
+                print("Invalid input! Please enter y for Yes, or n for No")
